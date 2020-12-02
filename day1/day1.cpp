8f6de1e81817d2f38855646a6592c9d1a0e4ec41
Iva Panić <ipanic@ekobit.hr>
2020-12-02 14:10:41 +0100

    solved day1

---
+++ day1/day1.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+constexpr int SUM = 2020;
+int main()
+{
+	std::string buffer;
+	std::ifstream stream("./input.txt");
+	std::vector<int> entries;
+
+	int entry, max_next_entry = 2020;
+	int current_line = 0;
+
+	while (stream >> entry)
+		entries.push_back(entry);
+
+	for (int entry : entries)
+	{
+		for (int second_entry : entries)
+		{
+			//PART 1
+			////starts here...
+			if (entry + second_entry == SUM)
+			{
+				std::cout << "" << entry * second_entry;
+				exit(0);
+			}
+			//...ends here
+
+			//PART2
+			//starts here...
+			for (int third_entry : entries)
+			{
+				if (entry + second_entry + third_entry == SUM)
+				{
+					std::cout << "" << entry * second_entry * third_entry;
+					exit(0);
+				}
+			}
+			//...ends here
+		}
+
+	}
+}
+
